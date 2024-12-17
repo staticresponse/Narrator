3b223c26aae22c85f4e1f516d4f33972b11b9bcd
Mike <36318669+staticresponse@users.noreply.github.com>
2024-12-17 16:15:33 -0500

    awesomesause

--- tts.py
+++ tts.py
@@ -8,11 +8,14 @@ import shutil
 import torch
 import time
 import multiprocessing as mp
-from nltk.tokenize import sent_tokenize
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
+from nltk.tokenize import PunktSentenceTokenizer
+import logging
 
-
+# Set up logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
 
 class TTSGenerator:
     def __init__(self, file_path, author, title, model=None, speaker_id=None):
@@ -40,12 +43,21 @@ class TTSGenerator:
             torch.cuda.is_available()
             and torch.cuda.get_device_properties(0).total_memory > 3500000000
         ):
-            print("Using GPU")
-            print(f"VRAM: {torch.cuda.get_device_properties(0).total_memory}")
+            logger.info("Using GPU")
+            logger.info(f"VRAM: {torch.cuda.get_device_properties(0).total_memory}")
             self.device = "cuda"
         else:
-            print("Not enough VRAM on GPU or CUDA not found. Using CPU") # purely for debug
+            logger.info("Not enough VRAM on GPU or CUDA not found. Using CPU") # purely for debug
             self.device = "cpu"
+        
+        self.config = {
+            'speaker': self.speaker_id,
+            'model_name': self.model,
+            'debug': self.debug,
+            'device': self.device,
+            'minratio': 0,
+            'engine_cl': None
+        }
 
     def generate_wav(self):
         """
@@ -56,6 +68,7 @@ class TTSGenerator:
 
         output_file = os.path.splitext(self.file_path)[0] + ".wav"
 
+        logger.info(f"Generating WAV file for {self.file_path}")
 
         if self.model == "tts_models/en/vctk/vits":
             self.generate_vits(output_file)
@@ -64,7 +77,7 @@ class TTSGenerator:
 
         self.validate_wav(output_file)
         self.add_metadata(output_file, self.author, self.title)
-        print(f"WAV file generated and saved at: {output_file}")
+        logger.info(f"WAV file generated and saved at: {output_file}")
 
         self.move_processed_file()
 
@@ -72,33 +85,24 @@ class TTSGenerator:
         """
         Generate a WAV file using the VITS model with speaker_id.
         """
+        logger.info("VITS Generation")
         with open(self.file_path, "r", encoding="utf-8") as file:
             text = file.read()
         if self.speaker_id is None:
-            raise ValueError("speaker_id must be provided for VITS model.")
-        print(f"Saving to {output_file}")
+            self.config['speaker'] = 'p364' # Have a default override just in case
+            logger.info("Speaker not set, defaulting to p364")
+        logger.info(f"Saving to {output_file}")
         sentance_chunk_length = 1000
         files = []
         position = 0
         start_time = time.time()
-
-        chapter_job_que = []
+        sentene_job_queue = []
+        chapter_job_queue = []
         tempfiles = []
-        config = {
-            'speaker': self.speaker,
-            'language': self.language,
-            'model_name': 'tts_models/en/vctk/vits',
-            'debug': self.debug,
-            'device': self.device,
-            'minratio': 0,
-            'engine_cl': None
-        }
         # initialize the tts engine
-        if 'debug' not in config:
-            self.debug = False
-        self.tts =  TTS(self.config['model_name']).to(self.device)
-
-        sentences = sent_tokenize(text)
+        self.tts =  TTS(self.config['model_name'])
+        tokenizer = PunktSentenceTokenizer()        
+        sentences = tokenizer.tokenize(text)
         sentences = [s for s in sentences if any(c.isalnum() for c in s)]
         sentence_groups = list(self.combine_sentences(sentences, sentance_chunk_length))
         for x in range(len(sentence_groups)):
@@ -110,17 +114,17 @@ class TTSGenerator:
                 continue
             retries = 2
             tempwav = "temp"+ "_" + str(x) + ".wav" # temp wavs of each sentence for future chunking
-            sentene_job_que.append((sentence_groups[x], tempwav))
+            sentene_job_queue.append((sentence_groups[x], tempwav))
             tempfiles.append(tempwav)
-        chapter_job_que.append(({'config': config, 'tempfiles': tempfiles, 'sentene_job_que': sentene_job_que, 'outputwav': outputwav}))
+        chapter_job_queue.append(({'config': self.config, 'tempfiles': tempfiles, 'sentene_job_queue': sentene_job_queue, 'output_file': output_file}))
 
-        print("initiating TTS Job")
-        if self.device == 'cuda':
-            map_result = list(map(process_book_chapter, chapter_job_que))
-        else:
-            pool = mp.Pool(processes=self.threads)
-            pool.map(process_book_chapter, chapter_job_que)
+        logger.info("Initiating TTS Job for VITS model")
+        for chapter in chapter_job_queue:
+            self.process_book_chapter(chapter)
 
+    def combine_sentences(self, sentences, length=1000):
+        for sentence in sentences:
+            yield sentence
 
     def generate_generic_tts(self, output_file):
         """
@@ -129,8 +133,59 @@ class TTSGenerator:
         tts = TTS(self.model)
         with open(self.file_path, "r", encoding="utf-8") as file:
             text = file.read()
+        logger.info(f"Generating WAV file using generic model for {self.file_path}")
         tts.tts_to_file(text=text, file_path=output_file)
 
+    def process_book_chapter(self, dat):
+        logger.info(f"Initiating chapter processing for output file: {dat['output_file']}")
+        tts_engine = TTS(dat['config']['model_name'])
+        
+        for text, file_name in dat['sentene_job_queue']:
+            retries = 2
+            while retries > 0:
+                try:
+                    # Force PyTorch tensors to detach and move to CPU
+                    with torch.no_grad():  # Ensures no autograd context
+                        try:
+                            if self.model == 'tts_models/en/vctk/vits':
+                                tts_engine.tts_to_file(text=text,speaker=self.config['speaker'], file_path=file_name)
+                            else:
+                                tts_engine.tts_to_file(text=text, file_path=file_name)
+                        except Exception as e:
+                            logger.error(f"Error generating {file_name}: {e}")
+                    break
+                except Exception as e:
+                    retries -= 1
+                    if retries == 0:
+                        logger.error(f"Failed to process text: {text}, error: {e}")
+                    else:
+                        logger.info(f"Retrying text synthesis for: {file_name}")
+
+        self.join_temp_files_to_chapter(dat['tempfiles'], dat['output_file'])
+        logger.info(f"Done processing chapter for output file: {dat['output_file']}")
+        return dat['output_file']
+
+    def join_temp_files_to_chapter(cls, tempfiles, output_file):
+        logger.info(f"Joining temporary WAV files into final chapter: {output_file}")
+        tempwavfiles = [AudioSegment.from_file(f"{f}") for f in tempfiles]
+        concatenated = sum(tempwavfiles)
+
+        one_sec_silence = AudioSegment.silent(duration=2000)
+        two_sec_silence = AudioSegment.silent(duration=3000)
+
+        audio_modified = AudioSegment.empty()
+        chunks = split_on_silence(concatenated, min_silence_len=2000, silence_thresh=-50)
+
+        for chunk in chunks:
+            audio_modified += chunk
+            audio_modified += one_sec_silence
+
+        audio_modified += two_sec_silence
+        audio_modified.export(output_file, format="wav")
+
+        for f in tempfiles:
+            os.remove(f)
+        logger.info(f"Chapter audio file saved at: {output_file}")
 
     def move_processed_file(self):
         """
@@ -151,23 +206,23 @@ class TTSGenerator:
         if os.path.exists(self.file_path):
             try:
                 shutil.move(self.file_path, txt_destination_path)
-                print(f"Text file moved to: {txt_destination_path}")
+                logger.info(f"Text file moved to: {txt_destination_path}")
             except Exception as e:
-                print(f"Error while moving text file '{self.file_path}': {e}")
+                logger.error(f"Error while moving text file '{self.file_path}': {e}")
 
         # Move audio file
         if os.path.exists(audio_file):
             try:
                 shutil.move(audio_file, audio_destination_path)
-                print(f"Audio file moved to: {audio_destination_path}")
+                logger.info(f"Audio file moved to: {audio_destination_path}")
             except Exception as e:
-                print(f"Error while moving audio file '{audio_file}': {e}")
+                logger.error(f"Error while moving audio file '{audio_file}': {e}")
 
     @staticmethod
     def validate_wav(wav_path):
         try:
             with wave.open(wav_path, 'rb') as wf:
-                print(f"WAV file validation successful: {wav_path}")
+                logger.info(f"WAV file validation successful: {wav_path}")
         except wave.Error as e:
             raise ValueError(f"Invalid WAV file generated: {e}")
 
@@ -177,50 +232,18 @@ class TTSGenerator:
         audio.tags["TITLE"] = [title]
         audio.tags["ARTIST"] = [author]
         audio.save()
-
-def join_temp_files_to_chapter(tempfiles, outputwav):
-    tempwavfiles = [AudioSegment.from_file(f"{f}") for f in tempfiles]
-    concatenated = sum(tempwavfiles)
-    # remove silence, then export to wav
-    #print(f"Replacing silences longer than 2 seconds with 2 seconds of silence ({outputwav})")
-    one_sec_silence = AudioSegment.silent(duration=2000)
-    two_sec_silence = AudioSegment.silent(duration=3000)
-    # This AudioSegment is dedicated for each file.
-    audio_modified = AudioSegment.empty()
-    # Split audio into chunks where detected silence is longer than 2 second
-    chunks = split_on_silence(
-        concatenated, min_silence_len=2000, silence_thresh=-50
-    )
-    # Iterate through each chunk
-    for chunkindex, chunk in enumerate(chunks):
-        audio_modified += chunk
-        audio_modified += one_sec_silence
-    # add extra 2sec silence at the end of each part/chapter
-    audio_modified += two_sec_silence
-    # Write modified audio to the final audio segment
-    audio_modified.export(outputwav, format="wav")
-    for f in tempfiles:
-        os.remove(f)
-
-def process_book_chapter(dat):
-    print("initiating chapter: ", dat['chapter'])
-    tts_engine = dat['config']['engine_cl'](dat['config'])
-    for text, file_name in dat['sentene_job_que']:
-        tts_engine.proccess_text_retry(text, file_name)
-    join_temp_files_to_chapter(dat['tempfiles'], dat['outputwav'])
-    print("done chapter: ", dat['chapter'])
-    return dat['outputwav']
+        logger.info(f"Metadata added to WAV file: {wav_path}")
 
 def process_queue(task_queue):
     while True:
         tts_task = task_queue.get()
         try:
+            logger.info(f"Processing task for file: {tts_task.file_path}")
             tts_task.generate_wav()
         except Exception as e:
-            print(f"Error processing task for file '{tts_task.file_path}': {e}")
+            logger.error(f"Error processing task for file '{tts_task.file_path}': {e}")
         finally:
             task_queue.task_done()
-
 
 # Initialize the task queue
 tts_queue = Queue()
