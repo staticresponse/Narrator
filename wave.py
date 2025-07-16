7c6154012f122841ce71eda6b87d5553ab25be8f
M <36318669+staticresponse@users.noreply.github.com>
2025-07-16 14:15:16 -0400

    kokorogenerator

--- wave.py
+++ wave.py
@@ -5,6 +5,15 @@ import logging
 from datetime import datetime
 from typing import Dict, Any
 from mutagen.wave import WAVE
+import spaces
+
+from kokoro import KModel, KPipeline
+#import misaki   #Just in case we need custom phonemes at a later date
+import gradio as gr
+import random
+import torch
+
+
 
 # Configure logger
 logger = logging.getLogger(__name__)
@@ -21,7 +30,8 @@ class WAVGenerator:
     ):
         if "name" not in model:
             raise ValueError("Model config must contain a 'name' field.")
-
+        if "sentence_chunk_length" not in model:
+            model['sentance_chunk_length'] == 1000 #default to 1000 chars if not specified
         self.file_path = file_path
         self.author = author
         self.title = title
@@ -34,6 +44,34 @@ class WAVGenerator:
             f"<WAVGenerator(title={self.title!r}, author={self.author!r}, "
             f"model={self.model.get('name')!r}, file_path={self.file_path!r}, created={self.creation_date!r})>"
         )
+    def extract_text(self):
+        with open(self.file_path, "r", encoding="utf-8") as file:
+            text = file.read()
+            return text
+
+    def combine_sentences(self, sentences, length=1000):
+        for sentence in sentences:
+            yield sentence
+
+    def sent_tokenizer(self):
+        '''
+            If the TTS generator wants a list of sentences
+        '''
+        text = self.extract_text()
+        sentence_queue = []
+        tokenizer = PunktSentenceTokenizer()
+        sentences = tokenizer.tokenize(text)
+        sentences = [s for s in sentences if any(c.isalnum() for c in s)]
+        sentence_groups = list(self.combine_sentences(sentences, model["sentence_chunk_length"]))
+        for x in range(len(sentence_groups)):
+            if len(sentence_groups[x]) == 0:
+                continue #skip if empty
+            if not any(char.isalnum() for char in sentence_groups[x]):
+                continue #skip if no chars or nums
+            tempwav = temp+"_"+str(x)+".wav" # temp wav for concatenator later
+            sentence_queue.append((sentence_groups[x],tempwav))
+        return sentence_queue #To be used by TTS Generation
+
 
     def apply_metadata(self, chapter_number: int):
         """
@@ -93,3 +131,24 @@ class WAVGenerator:
             logger.info(f"Combined WAV saved to {output_path}")
 
         except Exception as
+
+class KokoroGenerator(WAVGenerator):
+    '''
+    'bf_emma',
+    'bf_isabella',      
+    'bf_alice',
+    'bf_lily',
+    '🇬🇧 🚹 George': 'bm_george',
+    '🇬🇧 🚹 Fable': 'bm_fable',
+    '🇬🇧 🚹 Lewis': 'bm_lewis',
+    '🇬🇧 🚹 Daniel': 'bm_daniel',
+    '''
+    def generate_wav(self):
+        pipeline = KPipeline(lang_code='a')
+        generator = pipeline(text=sent_tokenizer(),voice='bm_lewis',speed=1,split_pattern=r'\n+')
+        for i, (gs,ps,audio) in enumerate(generator):
+            logger.info(f"index: {i}, text: {gs}, phonemes: {ps}")
+            sf.write(f'temp{i}.wav',audio,24000)
+    #combine_tem_wavs()#combine the temp wavs created above
+        
+
